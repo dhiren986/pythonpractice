b16ecd3a58cc467da47ebf0ced1a37a4d141159c
Dhiren Prajapati <dhirenprajapati1986@gmail.com>
2025-12-28 12:21:37 +0000

    Assignment - 1 is Submit as per Task with break line as expected

---
+++ assignment_task_1.py
@@ -0,0 +1,14 @@
+firstNumb=input("Enter the first number: ")
+secondNumb=input("enter the second number: ")
+
+addition=int(firstNumb)+int(secondNumb)
+subtraction=int(firstNumb)-int(secondNumb)
+multiplication=int(firstNumb)*int(secondNumb)
+division=int(firstNumb)/int(secondNumb)
+
+print("\nAddition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+
---
+++ assignment_task_2.py
@@ -0,0 +1,6 @@
+firstName=input("Enter your first name: ")
+secondName=input("enter your second name: ")
+
+fullName=firstName+" "+secondName
+
+print("Hello, " + fullName + "! Welcome to the Python program.")
