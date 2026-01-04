c3e36bd96ac3f16b2edbc6e5449d303d52da8fde
Dhiren Prajapati <dhirenprajapati1986@gmail.com>
2026-01-04 05:40:14 +0000

    Assignment - 2 is Submit as per Task with break line as expected

---
+++ assignment2_task_1.py
@@ -0,0 +1,6 @@
+oddEven = int(input("Enter a number: "))
+
+if oddEven % 2 == 0:
+    print(f"{oddEven} is an even number.")
+else:
+    print(f"{oddEven} is an odd number.")
---
+++ assignment2_task_2.py
@@ -0,0 +1,9 @@
+totalSum = 0
+
+fVal=1
+lVal = 51
+
+for num in range(fVal,lVal):
+    totalSum += num
+
+print(f"The sum of integers from {fVal} to {lVal} is:", totalSum)
